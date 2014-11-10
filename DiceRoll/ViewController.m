769eaba87ba6cb7aa4b879d2cc3ef9f7caf7e8c1
mjvoracek <mjvoracek@gmail.com>
2014-11-09 18:01:45 -0600

    banking points

--- DiceRoll/ViewController.m
+++ DiceRoll/ViewController.m
@@ -13,10 +13,11 @@
 @interface ViewController ()
 
 @property int roll1, roll2, roll3, roll4, roll5, roll6, sum;
+@property NSInteger totalScore;
 
 @end
 
-static NSString *RollsTakenKey = @"rollsTaken";
+static NSString *TotalScoreKey = @"totalScore";
 
 @implementation ViewController
 
@@ -24,65 +25,74 @@ static NSString *RollsTakenKey = @"rollsTaken";
 {
     [super viewWillAppear:animated];
     
-    [self showRollButtonValue];
+    NSUserDefaults *totalScoreDefault = [NSUserDefaults standardUserDefaults];
+    if ([totalScoreDefault objectForKey:TotalScoreKey] != nil) {
+        self.totalScore = [totalScoreDefault integerForKey:TotalScoreKey];
+    } else {
+        self.totalScore = 0;
+        [totalScoreDefault setInteger:self.totalScore forKey:TotalScoreKey];
+        [totalScoreDefault synchronize];
+    }
+    
+    [self updateTotalScore];
 }
 
-- (void)showRollButtonValue
+- (void)updateTotalScore
 {
-    NSInteger rollsTaken;
-    NSUserDefaults *rollsTakenDefault = [NSUserDefaults standardUserDefaults];
-    
-    if ([rollsTakenDefault objectForKey:RollsTakenKey] != nil) {
-        rollsTaken = [rollsTakenDefault integerForKey:RollsTakenKey];
-    } else {
-        rollsTaken = 1;
-        [rollsTakenDefault setInteger:rollsTaken forKey:RollsTakenKey];
-        [rollsTakenDefault synchronize];
-    }
-    [self updateButtonTitle:rollsTaken];
+    self.totalScoreLabel.text = [NSString stringWithFormat:@"Total Score: %ld", (long)self.totalScore];
+}
+
+- (void)updateRollScore
+{
+    self.sumLabel.text = [NSString stringWithFormat:@"Dice Total: %d", self.sum];
 }
 
 - (void)updateRollTaken:(NSMutableArray *)array
 {
     int sum = 0;
     ScoringData *scoringData = [[ScoringData alloc] init];
-    NSUserDefaults *rollsTakenDefault = [NSUserDefaults standardUserDefaults];
-    NSInteger rollsTaken = [rollsTakenDefault integerForKey:RollsTakenKey];
-    
-    if (rollsTaken < 3) {
-        rollsTaken++;
-        [rollsTakenDefault setInteger:rollsTaken forKey:RollsTakenKey];
-        [rollsTakenDefault synchronize];
-    } else {
-        rollsTaken = 1;
-        [rollsTakenDefault setInteger:rollsTaken forKey:RollsTakenKey];
-        [rollsTakenDefault synchronize];
-        //tried to fast enumerate, would not work
-        [self.firstDieView setAlpha:1.0];
-        [self.firstDieView setIsHeldDie:NO];
-        [self.secondDieView setAlpha:1.0];
-        [self.secondDieView setIsHeldDie:NO];
-        [self.thirdDieView setAlpha:1.0];
-        [self.thirdDieView setIsHeldDie:NO];
-        [self.fourthDieView setAlpha:1.0];
-        [self.fourthDieView setIsHeldDie:NO];
-        [self.fifthDieView setAlpha:1.0];
-        [self.fifthDieView setIsHeldDie:NO];
-        [self.sixthDieView setAlpha:1.0];
-        [self.sixthDieView setIsHeldDie:NO];
-    }
     
     sum = [scoringData checkDiceForScore:array];
     
-    self.sum = sum;
-    self.sumLabel.text = [NSString stringWithFormat:@"Dice Total: %d", self.sum];
-    [self updateButtonTitle:rollsTaken];
+    if (sum == self.sum) {
+        self.sum = 0;
+        [self resetDieViews];
+    } else {
+        self.sum = sum;
+    }
+    [self updateRollScore];
 }
 
-- (void)updateButtonTitle:(NSInteger)rollsTaken
+- (void) resetDieViews
 {
-    NSString *buttonTitle = [NSString stringWithFormat:@"Roll %ld", (long)rollsTaken];
-    [self.rollButton setTitle:buttonTitle forState:UIControlStateNormal];
+    //tried to fast enumerate, would not work
+    [self.firstDieView setAlpha:1.0];
+    [self.firstDieView setIsHeldDie:NO];
+    [self.secondDieView setAlpha:1.0];
+    [self.secondDieView setIsHeldDie:NO];
+    [self.thirdDieView setAlpha:1.0];
+    [self.thirdDieView setIsHeldDie:NO];
+    [self.fourthDieView setAlpha:1.0];
+    [self.fourthDieView setIsHeldDie:NO];
+    [self.fifthDieView setAlpha:1.0];
+    [self.fifthDieView setIsHeldDie:NO];
+    [self.sixthDieView setAlpha:1.0];
+    [self.sixthDieView setIsHeldDie:NO];
+}
+
+- (IBAction)bankButtonClicked:(id)sender
+{
+    NSUserDefaults *totalScoreDefault = [NSUserDefaults standardUserDefaults];
+    
+    self.totalScore += self.sum;
+    [totalScoreDefault setInteger:self.totalScore forKey:TotalScoreKey];
+    [totalScoreDefault synchronize];
+    
+    [self resetDieViews];
+    
+    self.sum = 0;
+    [self updateRollScore];
+    [self updateTotalScore];
 }
 
 - (IBAction)rollButtonClicked:(id)sender
